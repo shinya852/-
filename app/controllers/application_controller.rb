eeac489e7d19788f659ebb2acd14ba2ade768da5
shinya852 <shinya.june@gmail.com>
2021-10-15 14:28:37 +0000

    ログイン・アウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #デバイスが使用される前にconfigure_permitted_parametersが実行される
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #nameのデーター操作を許可上記ではユーザー登録の際にnameの操作が許可される
+  end
 end
